c64a582066610e54465eb81b3fcd378252d4fbdc
Afsha10 <unityofbeing1@gmail.com>
2023-09-24 20:44:07 +0100

    Created Node server and made it work
    * Created server.js
    * Installed relevant packages
    * added database credentials and .env file
    * created git gitignore and put .env and node_modules
    * added a GET endpoint
    * made the endpoint work in POSTMAN

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+// credentials for database
+const db = new Pool({
+  user: process.env.POSTGRES_USERNAME,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  // ssl: true,
+});
+
+// GET "/"
+
+// Connecting to database
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// This endpoint is used to get all the sessions using SQL queries
+
+app.get("/sessions", (req, res) => {
+  db.query(`SELECT * FROM sessions ORDER BY date`)
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).send("Database Error");
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
