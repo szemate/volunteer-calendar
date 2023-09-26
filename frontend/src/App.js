e5913665ef9c5e6833cd3416e28da4d932d5323b
Afsha10 <unityofbeing1@gmail.com>
2023-09-26 04:54:57 +0100

    Created a simple schedule calendar using material-ui library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import SlotsCalendar from "./components/SlotsCalendar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SlotsCalendar />
     </div>
   );
 }
---
+++ frontend/src/components/SlotsCalendar.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import sessions from "../sessionsdata.json";
+
+const columns = [
+  { id: "formatted_date", label: "Date", minWidth: 30 },
+  // { id: "day", label: "Day", minWidth: 10 },
+  { id: "morning shift", label: "Morning Shift", minWidth: 10 },
+  { id: "evening shift", label: "Evening Shift", minWidth: 10 },
+  { id: "volunteer id", label: "Volunteer ID", minWidth: 10 },
+];
+
+function SlotsCalendar() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(28);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <div>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 700 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sessions
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, rowIndex) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={rowIndex} // Use rowIndex as the key
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align="left">
+                            {value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[28, 62, 64]}
+          component="div"
+          count={sessions.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+}
+
+export default SlotsCalendar;
