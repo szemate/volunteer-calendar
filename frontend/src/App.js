f1426719266c46f70df323126791e21240a00348
Afsha10 <unityofbeing1@gmail.com>
2023-09-26 05:10:07 +0100

    Added a Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import SlotsCalendar from "./components/SlotsCalendar";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SlotsCalendar />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="mt-5 bg-black p-4 text-center text-xs text-white">
+        <p>&copy; 2023 YoutubeVault Created by Afsha with ❤️</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
