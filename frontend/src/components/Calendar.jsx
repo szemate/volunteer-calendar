82952d4a96fd80440a75b7cf256ad98fe0373e63
Afsha10 <unityofbeing1@gmail.com>
2023-09-28 23:25:37 +0100

    Added an information box about the current date and made it mobile responsive

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -22,26 +22,45 @@ function Calendar() {
   //   ];
 
   return (
-    <div className="w-96 h-96 ">
-      <div className="w-full grid grid-cols-7">
-        {days.map((day, index) => {
-          return (
-            <h1 key={index} className="h-14 grid place-content-center text-sm">
-              {day}
-            </h1>
-          );
-        })}
-      </div>
-      <div className="w-full grid grid-cols-7">
-        {generateDate().map(({ date, currentMonth, today }, index) => {
-          return (
-            <div key={index} className="h-14 border grid place-content-center text-sm">
-              <h1 className={conditions(currentMonth ? "" : "text-gray-400", today ? "bg-red-600 text-white" : "", "h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all cursor-pointer")}>
-                {date.date()}
+    <div className="flex flex-col sm:flex-row sm:w-1/2 sm:mx-auto sm:divide-x-2 sm:gap-10 h-screen items-start ">
+      <div className="w-96 h-96 ">
+        <div className="w-full grid grid-cols-7 text-gray-700">
+          {days.map((day, index) => {
+            return (
+              <h1
+                key={index}
+                className="h-14 grid place-content-center text-sm"
+              >
+                {day}
               </h1>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="w-full grid grid-cols-7">
+          {generateDate().map(({ date, currentMonth, today }, index) => {
+            return (
+              <div
+                key={index}
+                className="h-14 border grid place-content-center text-sm"
+              >
+                <h1
+                  className={conditions(
+                    currentMonth ? "" : "text-gray-400",
+                    today ? "bg-red-600 text-white" : "",
+                    "h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all cursor-pointer"
+                  )}
+                >
+                  {date.date()}
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="h-96 w-96 mt-5 sm:px-5">
+        <h1>Sessions Status for 29th Sep 2023</h1>
+        <p>Morning Session: Booked by someone</p>
+        <p>Evening Session: You've booked it</p>
       </div>
     </div>
   );
