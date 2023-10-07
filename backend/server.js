36ac484b3253b2308a0b2d2bfb00bc37dac3799a
Afsha10 <unityofbeing1@gmail.com>
2023-10-07 12:26:39 +0100

    Added session_id to sesionsdata.json

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const db = new Pool({
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
+  // ssl: process.env.POSTGRES_SSL === "true" ? { rejectUnauthorized: false } : false
   ssl: true,
 });
 
